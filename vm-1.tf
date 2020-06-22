c8a2679b4b9c6c9349819767fdcd3ab8c7f0faa3
Nick <nick@dev.int.aoeu.uk>
2020-06-22 20:13:57 +0100

    Start customising VM

--- vm-1.tf
+++ vm-1.tf
@@ -22,52 +22,44 @@ resource "azurerm_network_interface" "vm_1_nic" {
 
 # Connect the security group to the network interface
 resource "azurerm_network_interface_security_group_association" "vm_1_nic_sg" {
-    network_interface_id      = azurerm_network_interface.vm_1_nic.id
-    network_security_group_id = azurerm_network_security_group.front_end_sg.id
+  network_interface_id      = azurerm_network_interface.vm_1_nic.id
+  network_security_group_id = azurerm_network_security_group.front_end_sg.id
 }
 
+# Create the VM!
+resource "azurerm_linux_virtual_machine" "vm_1" {
+  name                  = "vm_1"
+  location              = var.az_region
+  resource_group_name   = azurerm_resource_group.status_page_rg.name
+  network_interface_ids = [azurerm_network_interface.vm_1_nic.id]
+  # Pretty small please, I'm not made of money!
+  size                  = "Standard_B1s"
 
+  os_disk {
+    name              = "myOsDisk"
+    caching           = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
 
+  source_image_reference {
+    publisher = "Debian"
+    offer     = "debian-10"
+    sku       = "10"
+    version   = "latest"
+  }
 
-
-resource "tls_private_key" "example_ssh" {
-  algorithm = "RSA"
-  rsa_bits = 4096
-}
-
-output "tls_private_key" { value = "tls_private_key.example_ssh.private_key_pem" }
-
-resource "azurerm_linux_virtual_machine" "myterraformvm" {
-    name                  = "myVM"
-    location              = var.az_region
-    resource_group_name   = azurerm_resource_group.status_page_rg.name
-    network_interface_ids = [azurerm_network_interface.vm_1_nic.id]
-    size                  = "Standard_B1s"
-
-    os_disk {
-        name              = "myOsDisk"
-        caching           = "ReadWrite"
-        storage_account_type = "Standard_LRS"
-    }
-
-    source_image_reference {
-        publisher = "Canonical"
-        offer     = "UbuntuServer"
-        sku       = "16.04.0-LTS"
-        version   = "latest"
-    }
-
-    computer_name  = "myvm"
-    admin_username = "azureuser"
-    disable_password_authentication = true
+  computer_name  = "vm_1"
+  admin_username = "azureuser"
+  disable_password_authentication = true
         
-    admin_ssh_key {
-        username       = "azureuser"
-        public_key     = tls_private_key.example_ssh.public_key_openssh
-    }
+  admin_ssh_key {
+    username       = "azureuser"
+    # No ECDSA support on Azure yet, apparently!
+    public_key     = file("~/.ssh/id_rsa.pub")
+  }
 
-    boot_diagnostics {
-        storage_account_uri = azurerm_storage_account.status_page_sa.primary_blob_endpoint
-    }
+  boot_diagnostics {
+    storage_account_uri = azurerm_storage_account.status_page_sa.primary_blob_endpoint
+  }
 }
 
