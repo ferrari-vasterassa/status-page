4f9b5b30fb75875db1faf4574a93abbcad2b45b1
Nick <nick@dev.int.aoeu.uk>
2020-06-22 18:23:18 +0100

    Everything updated properly this time?

--- network.tf
+++ network.tf
@@ -1,9 +1,19 @@
 # Resource group network
-resource "azurerm_virtual_network" "status-page-nw" {
-  name                = "status-page"
+resource "azurerm_virtual_network" "virtual-nw" {
+  name                = "status-page-nw"
   resource_group_name = azurerm_resource_group.status-page-rg.name
   location            = azurerm_resource_group.status-page-rg.location
-  # We only need a few addresses.  Let's pick a random address space unlikely to clash with anything else we deploy in future in this RG
+  # Let's pick a random address space unlikely to clash with anything else we deploy in future in this RG or elsewhere.
   address_space       = ["10.34.85.0/24"]
+}
+
+
+# Status page network
+resource "azurerm_subnet" "status-page-sn" {
+  name                 = "status-page-sn"
+  resource_group_name  = azurerm_resource_group.status-page-rg.name
+  virtual_network_name = azurerm_virtual_network.virtual-nw.name
+  # Acutal subnet we'll use.  Doesn't need to be big.
+  address_prefix       = "10.34.85.0/28"
 }
 
