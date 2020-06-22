4ae98093b975506b35f410d3c105340e07e8e78c
Nick <nick@dev.int.aoeu.uk>
2020-06-22 18:32:17 +0100

    Grab a public address

--- network.tf
+++ network.tf
@@ -7,7 +7,6 @@ resource "azurerm_virtual_network" "virtual-nw" {
   address_space       = ["10.34.85.0/24"]
 }
 
-
 # Status page network
 resource "azurerm_subnet" "status-page-sn" {
   name                 = "status-page-sn"
@@ -15,5 +14,13 @@ resource "azurerm_subnet" "status-page-sn" {
   virtual_network_name = azurerm_virtual_network.virtual-nw.name
   # Acutal subnet we'll use.  Doesn't need to be big.
   address_prefix       = "10.34.85.0/28"
+}
+
+# Public address
+resource "azurerm_public_ip" "ip-1" {
+  name                         = "ip-1"
+  location                     = var.az_region
+  resource_group_name          = azurerm_resource_group.status-page-rg.name
+  allocation_method            = "Dynamic"
 }
 
