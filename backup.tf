4447dbfcf0ab7f5c78e46de1aebe6f6fa437205d
Nick <nick@dev.int.aoeu.uk>
2020-06-23 22:45:56 +0100

    Re-enable backups

---
+++ backup.tf
@@ -0,0 +1,25 @@
+# Create a backup vault
+resource "azurerm_recovery_services_vault" "status_page_backup_vault" {
+  # Name can't include underscores
+  name                = "status-page-backup-vault"
+  location            = azurerm_resource_group.status_page_rg.location
+  resource_group_name = azurerm_resource_group.status_page_rg.name
+  sku                 = "Standard"
+}
+
+# Create a backup policy
+resource "azurerm_backup_policy_vm" "status_page_backup_policy" {
+  name                = "status_page_backup_policy"
+  resource_group_name = azurerm_resource_group.status_page_rg.name
+  recovery_vault_name = azurerm_recovery_services_vault.status_page_backup_vault.name
+
+  backup {
+    frequency = "Daily"
+    time      = "23:00"
+  }
+
+  retention_daily {
+    count = 10
+  }
+}
+
